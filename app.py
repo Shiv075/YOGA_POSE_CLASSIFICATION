a36ddae616777c5a6794966696cc6aa45fe1c1ec
Shiv075 <141173782+Shiv075@users.noreply.github.com>
2025-09-28 19:13:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,6 @@ def predict():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    port = int(os.environ.get("PORT", 4000))  # Render provides PORT
     app.run(host="0.0.0.0", port=port, debug=False)
+
